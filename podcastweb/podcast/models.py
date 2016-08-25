9443c077baed9b5eb83b181aeba9a92a3b97df4e
Tim Gupta <timg@surveymonkey.com>
2016-08-25 13:58:01 -0700

    Add episode model and register it to the admin.

--- podcastweb/podcast/models.py
+++ podcastweb/podcast/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Episode(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    summary = models.CharField(max_length=255)
+    duration = models.IntegerField()
+    keywords = models.CharField(max_length=255)  # TODO: maybe store this as one to many in a separate table
+    image_url = models.CharField(max_length=255) # TODO: switch to using ImageField and handle upload stuff
+    description = models.CharField(max_length=255)
+    filename = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
