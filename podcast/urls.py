58b81ac79c108e37c118d9e6af31d50cef8f10b8
Davinder Mahal <davinder@mahal.org>
2016-08-27 12:07:39 -0700

    Add bootstrap template to each page

--- podcast/urls.py
+++ podcast/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.episode_list),
+    url(r'^$', views.episode_list, name="episode_list"),
     url(r'^episode/(?P<episode_id>\d+)/$', views.episode_detail, name="episode_detail"),
 ]
