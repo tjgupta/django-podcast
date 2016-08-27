1ac38d5594809eaad0040947776306503b9ceb3a
Davinder Mahal <davinder@mahal.org>
2016-08-27 12:39:09 -0700

    Add Podcast and iTunes metadata models

--- podcast/admin.py
+++ podcast/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Episode
+from .models import Episode, Podcast, ItunesMetaData
 
 # Register your models here.
 admin.site.register(Episode)
+admin.site.register(Podcast)
+admin.site.register(ItunesMetaData)
--- podcast/models.py
+++ podcast/models.py
@@ -4,7 +4,35 @@ from django.db import models
 from django.utils import timezone
 
 
+class Podcast(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+    language = models.CharField(max_length=10)
+    copyright = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    webmaster = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class ItunesMetaData(models.Model):
+    podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
+    author = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    summary = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    image_url = models.CharField(max_length=255)  # TODO: switch to using ImageField and handle upload stuff
+    owner = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.author
+
+
 class Episode(models.Model):
+    podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     subtitle = models.CharField(max_length=255)
