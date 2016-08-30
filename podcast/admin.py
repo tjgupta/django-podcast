70a426049b5f5f96e8139921d4b5e78332b09b73
Tim Gupta <timg@surveymonkey.com>
2016-08-30 11:20:51 -0700

    Fix inline admin for Podcast and ItunesMetaData.

--- podcast/admin.py
+++ podcast/admin.py
@@ -3,24 +3,20 @@ from django.contrib import admin
 from .models import Episode, Podcast, ItunesMetaData, Video, Audio, Keyword
 
 # Register your models here.
-# admin.site.register(PodcastInline)
-# admin.site.register(ItunesMetaData)
-# admin.site.register(Keyword)
-# admin.site.register(Episode)
-# admin.site.register(Video)
-# admin.site.register(Audio)
-#
+admin.site.register(Keyword)
+admin.site.register(Episode)
+admin.site.register(Video)
+admin.site.register(Audio)
 
 
-class PodcastInline(admin.TabularInline):
-    model = Podcast
-    fk_name = 'podcast'
+class ItunesMetaDataAdmin(admin.TabularInline):
+    model = ItunesMetaData
 
 
-class ItunesMetaDataAdmin(admin.ModelAdmin):
+class PodcastAdmin(admin.ModelAdmin):
     inlines = [
-        PodcastInline,
+        ItunesMetaDataAdmin,
     ]
 
 
-admin.site.register(PodcastInline)
+admin.site.register(Podcast, PodcastAdmin)
