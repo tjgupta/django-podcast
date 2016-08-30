5ebdae546bf9917813fe4b2cfecaab7d7856556d
Davinder Mahal <davinder@mahal.org>
2016-08-30 11:28:03 -0700

    Simplify models with basic data dump

--- podcast/admin.py
+++ podcast/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 
-from .models import Episode, Podcast, ItunesMetaData, Video, Audio, Keyword
+from .models import Episode, Podcast, Video, Audio, Keyword
 
-# Register your models here.
 admin.site.register(Podcast)
-admin.site.register(ItunesMetaData)
 admin.site.register(Keyword)
 admin.site.register(Episode)
 admin.site.register(Video)
