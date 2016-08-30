3397d852a4684947710bb76649fb27d761812570
Davinder Mahal <davinder@mahal.org>
2016-08-30 10:20:52 -0700

    WIP: Add PodcastInline but contains error message

--- podcast/admin.py
+++ podcast/admin.py
@@ -3,9 +3,24 @@ from django.contrib import admin
 from .models import Episode, Podcast, ItunesMetaData, Video, Audio, Keyword
 
 # Register your models here.
-admin.site.register(Podcast)
-admin.site.register(ItunesMetaData)
-admin.site.register(Keyword)
-admin.site.register(Episode)
-admin.site.register(Video)
-admin.site.register(Audio)
+# admin.site.register(PodcastInline)
+# admin.site.register(ItunesMetaData)
+# admin.site.register(Keyword)
+# admin.site.register(Episode)
+# admin.site.register(Video)
+# admin.site.register(Audio)
+#
+
+
+class PodcastInline(admin.TabularInline):
+    model = Podcast
+    fk_name = 'podcast'
+
+
+class ItunesMetaDataAdmin(admin.ModelAdmin):
+    inlines = [
+        PodcastInline,
+    ]
+
+
+admin.site.register(PodcastInline)
