b99609fc956a8aa997f71b508dc3a1e9a11a4f7d
Tim Gupta <timg@surveymonkey.com>
2016-08-30 22:19:13 -0700

    Add in proper query for feed items.

--- podcast/views.py
+++ podcast/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.utils import formats
 import datetime
-from .models import Episode, Podcast
+from .models import Episode, Podcast, Media
 
 
 def episode_list(request):
@@ -17,15 +17,11 @@ def episode_detail(request, episode_id):
 
 def podcast_feed(request, podcast_id):
     podcast = get_object_or_404(Podcast, pk=podcast_id)
-    # episodes = Episode.objects.filter(podcast_id=podcast_id).select_related()
-    # episodes = Episode.objects.with_media(podcast_id=podcast_id)
-    episodes = podcast.episode_set.all().filter(media__format=1)
-    print(episodes)
-
+    items = Media.objects.filter(episode=1, format=Media.AUDIO).select_related('episode').filter(episode__podcast=podcast_id)
     last_build_date = formats.date_format(datetime.datetime.now(), "D, d M Y H:i:s O")
 
     return render(request, 'feed.xml', {
         'podcast': podcast,
-        'episodes': episodes,
+        'items': items,
         'last_build_date': last_build_date
-    })
+    }, content_type='application/xhtml+xml')
